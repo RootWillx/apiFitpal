24d50e188ad7d3eb8694caa3471e880ce1f89a75
RootWillx <metalbacom@gmail.com>
2023-02-20 12:56:16 -0500

    Actualización de seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,6 +13,34 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'Alex Oscar',
+                'password' => 'asd123',
+                'age' => 28,
+                'gender' => 'Masculino',
+                'identification' => 70218511,
+                'high' => 1.8,
+                'weight' => 72
+            ],
+            [
+                'name' => 'María Perla',
+                'password' => 'asd123',
+                'age' => 34,
+                'gender' => 'Femenino',
+                'identification' => 80218522,
+                'high' => 1.5,
+                'weight' => 50
+            ],
+            [
+                'name' => 'Cielo Celeste',
+                'password' => 'asd123',
+                'age' => 50,
+                'gender' => 'Femenino',
+                'identification' => 23888591,
+                'high' => 1.6,
+                'weight' => 60
+            ]
+        ]);
     }
 }
